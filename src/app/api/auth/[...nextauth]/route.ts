adaf04497f87c98673628e1a94681b8d3ac87a55
Wesley Sum <wesleysumtzehern@.gmail.com>
2024-06-02 23:55:13 -0400

    fix google auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,6 +7,3 @@ const handler = NextAuth(authConfig)
 
 export { handler as GET, handler as POST }
 
-function GitHubProvider(arg0: { clientId: string; clientSecret: string; }): import("next-auth/providers/index").Provider {
-    throw new Error("Function not implemented.");
-}
